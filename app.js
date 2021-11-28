8f0aef397b2eeefe07af3e98281c7ff2723a6102
Melvin Nacfil <mnacfil@gmail.com>
2021-11-28 23:56:53 +0800

    Finish Personal Develope Website/Capstone 1 Project

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+// const projectItem = document.querySelectorAll('.project-item');
+
+// 	projectItem.forEach(function(bg) {
+
+// 		bg.addEventListener('mouseover', function(){
+// 			projectItem.classList.add('project-item-darken')
+// 		});
+// 	});
+
+    // const projectItem = document.querySelectorAll('.project-item')
+
+    // projectItem.forEach(function(projectItem) {
+    //     projectItem.addEventListener('mouseover', function (){
+    //         console.log(projectItem.classList);
+    //         projectItem.classList.add('project-item-darken');
+    //     })
+
+        
+    //     projectItem.addEventListener('mouseout', function(){
+    //         projectItem.classList.remove('project-item-darken');
+    //     })
+    // });
